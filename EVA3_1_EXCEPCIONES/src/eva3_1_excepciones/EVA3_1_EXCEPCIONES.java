b6ca95c7c4a90de4437d23adb4ea2356695935d6
Tashiro13 <143568865+Tashiro13@users.noreply.github.com>
2024-04-23 18:12:15 -0600

    Add files via upload

---
+++ EVA3_1_EXCEPCIONES/src/eva3_1_excepciones/EVA3_1_EXCEPCIONES.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_1_excepciones;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alex_
+ */
+public class EVA3_1_EXCEPCIONES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner captu = new Scanner(System.in);
+        int num;
+        System.out.println("Introduce un valor entero: ");
+        num = captu.nextInt();
+        System.out.println("El valor es " + num);
+        
+    }
+    
+}
